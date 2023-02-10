6e855d5c09bca3c302bcc35aec391f562c813d77
Alexseptt100 <alan03002@utb.vaxjo.se>
2023-02-10 10:09:36 +0100

    Add project files.

---
+++ Uppgift6_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Common;
+
+namespace uppgift6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Störst(6,6);
+        }
+        static void Störst(int tal1, int tal2)
+        {
+            if(tal1 > tal2)
+            {
+                Console.WriteLine("Den största talen är" + tal1 + ".");
+            }
+            else if(tal1 < tal2)
+            {
+                Console.WriteLine("Den största talen är" + tal2 + ".");
+            }
+            else if(tal1 == tal2)
+            {
+                Console.WriteLine("De är lika stora");
+            }
+        }
+    }
+}
